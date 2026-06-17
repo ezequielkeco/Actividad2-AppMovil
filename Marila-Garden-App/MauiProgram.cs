729e1ee94945be8471855ed48e0cb0de3802c428
Ezequiel Santos <87910910+ezequielkeco@users.noreply.github.com>
2026-06-17 04:37:40 -0400

    Unidad 2.1
    Se realizaron correcciones de estilos globales y ajustes de centralizado de textos. También, se añadieron sombras a las tarjetas y se eliminaron las líneas en los placeholders de los campos en los formularios de inicio de sesión y solicitud de servicios.

--- Marila-Garden-App/MauiProgram.cs
+++ Marila-Garden-App/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Handlers;
 
 namespace Marila_Garden_App
 {
@@ -7,17 +8,35 @@ namespace Marila_Garden_App
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-#if DEBUG
-    		builder.Logging.AddDebug();
+            builder.ConfigureMauiHandlers(handlers =>
+            {
+#if ANDROID
+
+                EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+                {
+                    handler.PlatformView.Background = null;
+                });
+
+                PickerHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+                {
+                    handler.PlatformView.Background = null;
+                });
+
+                DatePickerHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+                {
+                    handler.PlatformView.Background = null;
+                });
+
 #endif
+            });
 
             return builder.Build();
         }
